b3e64e5ff5f1ce5f530134471a1d500f87c6bfe5
Jerry Shan <jiarui.shan@berkeley.edu>
2019-02-11 20:05:03 -0800

    Initial Commit

---
+++ BerkeleyCloud/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BerkeleyCloud
+//
+//  Created by Jia Rui Shan on 2019/2/11.
+//  Copyright © 2019 UC Berkeley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
