4714a4d7225dde45bf83eb68937ea4c8f7c57b45
Jerry Shan <jiarui.shan@berkeley.edu>
2019-02-13 08:16:36 -0800

    Attempt to merge with remote

---
+++ BerkeleyCloud/TabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  TabBarController.swift
+//  BerkeleyCloud
+//
+//  Created by Jia Rui Shan on 2019/2/11.
+//  Copyright © 2019 UC Berkeley. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# BerkeleyCloud
+Intends to be a mobile platform to access and manage the SFTP storage space that OCF provides.
